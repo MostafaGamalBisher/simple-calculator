e6d7fe1c810b402b94e4c044519aa15a3a7680e2
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-03-16 21:40:23 +0300

    adding toggle activation functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
-import { toggleClass } from './utils.js';
+import { toggleClass, pressToggle } from './utils.js';
 
 const toggleButton = document.querySelector('.themes__toggle');
 
 toggleButton.addEventListener('click', () => {
   toggleClass(toggleButton, 'themes__toggle--isActive');
 });
+
+pressToggle(toggleButton, 'themes__toggle--isActive');
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,11 @@
 export const toggleClass = (element, toggledClass) => {
   element.classList.toggle(toggledClass);
 };
+
+export const pressToggle = (element, toggledClass) => {
+  element.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      toggleClass(element, toggledClass);
+    }
+  });
+};
