fef10e5434e235d66633d42a46439404c43072c4
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-03-17 02:10:01 +0300

    adding displaying numbers function and validating adding . + 0

--- src/js/display.js
+++ src/js/display.js
@@ -1,9 +1,23 @@
-const resultElement = document.querySelector('.calc__result');
-console.log(resultElement);
-const numbersElements = document.querySelectorAll('[data-type]');
+let storedNumber = '';
+let currentNumber = '';
+let operation = '';
 
-const printElemen = () => {
-  numbersElements.addEventListner('click', () => {
-    numbersElements.dataset.type === 'number' && console.log('hello world!');
+const resultElement = document.querySelector('.calc__result');
+
+export const updateScreen = (vlaue) => {
+  resultElement.textContent = !vlaue ? '0' : vlaue;
+};
+
+export const numbersHandler = (value) => {
+  if (value === '.' && currentNumber.includes('.')) return;
+  if (value === '0' && !currentNumber) return;
+
+  currentNumber += value;
+  updateScreen(currentNumber);
+};
+
+export const keyboardNumbersDisplaying = (element, dataType) => {
+  element.addEventListener('click', () => {
+    element.dataset.type === dataType && numbersHandler(element.dataset.value);
   });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 import { toggleClass, pressToggle } from './utils.js';
+import { keyboardNumbersDisplaying } from './display.js';
 
 const toggleButton = document.querySelector('.themes__toggle');
+const numbersElements = document.querySelectorAll('[data-type]');
 
 toggleButton.addEventListener('click', () => {
   toggleClass(toggleButton, 'themes__toggle--isActive');
 });
 
 pressToggle(toggleButton, 'themes__toggle--isActive');
+
+numbersElements.forEach((element) => {
+  keyboardNumbersDisplaying(element, 'number');
+});
